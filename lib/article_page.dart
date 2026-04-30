1f125860e3674048e24908c96a37b83a6774edca
littlehorseboy <otis@skyeyes.tw>
2026-04-30 10:52:00 +0800

    feat: Enhance ArticleView to display loading state and errors, and add ArticlePage for article content

---
+++ lib/article_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'summary.dart';
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [Text('Article content will be displayed here...')],
+      ),
+    );
+  }
+}
--- lib/article_view.dart
+++ lib/article_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'article_model.dart';
+import 'article_page.dart';
 import 'article_view_model.dart';
 
 class ArticleView extends StatelessWidget {
@@ -17,7 +18,20 @@ class ArticleView extends StatelessWidget {
       body: ListenableBuilder(
         listenable: viewModel,
         builder: (context, child) {
-          return const Center(child: Text('Loading...'));
+          return switch ((
+            viewModel.isLoading,
+            viewModel.summary,
+            viewModel.error,
+          )) {
+            (true, _, _) => const CircularProgressIndicator(),
+            (_, _, final Exception e) => Text('Error: $e'),
+            // The summary must be non-null in this switch case.
+            (_, final summary?, _) => ArticlePage(
+              summary: summary,
+              nextArticleCallback: viewModel.fetchArticle,
+            ),
+            _ => const Text('Something went wrong!'),
+          };
         },
       ),
     );
